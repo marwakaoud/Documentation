c9c46ffef04d25d61b97aad5ee438826d30313bd
mohamed gamal <m.yassen.93@gmail.com>
2020-09-18 13:09:14 +0200

    check errors 2

--- src/Pages/LoginStepTwo/LoginStepTwoPage.jsx
+++ src/Pages/LoginStepTwo/LoginStepTwoPage.jsx
@@ -3,7 +3,7 @@ import SignInPass from "../../Components/SignInPass/SignInPass";
 
 const LoginStepTwoPage = props =>  {
     return (<div>
-        <SignInPass username={this.props.location.state.username} fullName={this.props.location.state.full_name}/>
+        <SignInPass username={props.location.state.username} fullName={props.location.state.full_name}/>
     </div>)
 }
 
