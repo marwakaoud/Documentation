5240cf7c46314252404c0424a17f3bac3e90c6c4
zeinab <zeinab@gmail.com>
2020-09-26 05:45:54 -0700

    zeze

--- src/Pages/landing_pages/landing.jsx
+++ src/Pages/landing_pages/landing.jsx
@@ -1,20 +1,20 @@
 import React , {Component} from 'react';
 import axios from 'axios';
 import '../landing_pages/landing.css';
+import { usePagination } from '@material-ui/lab/Pagination';
 class Landing extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data:[], url: 'http://172.16.1.102/api/v1/getdata'
+            data:[], url: 'http://172.16.1.102:6060/api/v1/getdata'
         }
 }
 componentDidMount(){
     axios({
-        method: 'post',
+        method: 'POST',
         url:this.state.url,
-        timeout: 4000,   
         data:{
-            fun_name:"FU_RES_YEAR",
+            fun_name:"FU_DOC_APPLICATIONS",
             param_name:[],
             param_value:[],
         }
@@ -24,6 +24,16 @@ componentDidMount(){
          this.setState({data :response.data.Table })
         })
       .catch(error => console.error('timeout exceeded'))
+}
+handleClick =(id) =>{
+  // alert(id);
+  const all_data = this.state.data;
+ const single_data = all_data.find(d => d.APP_DESC === id);
+ const par = document.getElementById("1");
+ const par_container = document.getElementsByClassName("zeinab");
+ par_container[0].style.display ="block"
+ par.textContent = single_data.APP_DESC
+ 
 }
 
 render () {
@@ -41,10 +51,13 @@ render () {
 <img src="https://api.adorable.io/avatars/150/1"/>
       </div>
       <div className="back">
-     <p>{d.ID}</p>
-     <p>{d.NAME}</p>
-     <p>{d.CURRENT_FLAG}</p>
+   
+     <p>{d.APP_NAME}</p>
+     
+     <button onClick={ () =>  this.handleClick(d.APP_DESC)}>DESC</button>
+
       </div>
+      
     </div>
   </div>
   </div>
@@ -52,6 +65,13 @@ render () {
      )
  })
  }
+</div>
+
+<div className="zeinab">
+  <p id="1">
+    
+  </p>
+
 </div>
   </div>
     )
